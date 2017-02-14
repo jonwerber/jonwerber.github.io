c6ddb053291533825fd8ff54874c65931a20fe7b
Jonathan Werber <jonathanlouiswerber@gmail.com>
2017-02-14 18:37:41 -0500

    console logs removed

--- news/scripts.js
+++ news/scripts.js
@@ -27,11 +27,11 @@ app.controller('myCtrl', function ($scope, $http) {
             url: 'https://newsapi.org/v1/articles?source=' + sources[i] + '&sortBy=top&apiKey=79b5eacee55a4e8d89ed43a73add9edf'
         }).then(function successCallback(response) {
             Array.prototype.push.apply($scope.articles, response.data.articles);
-            console.log($scope.articles);
-            console.log(response.data.articles);
+        //    console.log($scope.articles);
+        //    console.log(response.data.articles);
 
         }, function errorCallback(response) {
-            console.log("ERROR + " + response)
+        a    console.log("ERROR + " + response)
         });
     }
 });

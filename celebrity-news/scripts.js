e0d53b0e6336308030abe3d305cbbf87df13c015
Jonathan Werber <jonathanlouiswerber@gmail.com>
2017-02-15 19:37:35 -0500

    closer

--- celebrity-news/scripts.js
+++ celebrity-news/scripts.js
@@ -23,49 +23,48 @@ app.controller('mainCtrl', function ($scope, $http) {
 
     $scope.getMtvNews = function() {
         $scope.mtvNews = [];
-        getNews('mtv-news', $scope.mtvNews );
+        getNews('mtv-news', $scope.mtvNews, 'latest' );
     }
-
+// set image as background image (ie. set height based on image proportions so you can add text over the image -->
     $scope.getMtvNewsUk = function() {
         $scope.mtvNewsUk = [];
-        getNews('mtv-news-uk', $scope.mtvNewsUk );
+        getNews('mtv-news-uk', $scope.mtvNewsUk, 'top' );
     }
 
     $scope.getIgn = function() {
         $scope.ign = [];
-        getNews('ign', $scope.ign );
+        getNews('ign', $scope.ign, 'latest' );
     }
 
     $scope.getMirror = function() {
         $scope.mirror = [];
-        getNews('mirror', $scope.mirror );
+        getNews('mirror', $scope.mirror, 'latest' );
     }
 
     $scope.getHuffPo = function() {
         $scope.huffPo = [];
-        getNews('the-huffington-post', $scope.huffPo );
+        getNews('the-huffington-post', $scope.huffPo, 'top' );
     }
+
     $scope.getReddit = function() {
         $scope.reddit = [];
-        getNews('reddit-r-all', $scope.reddit );
+        getNews('reddit-r-all', $scope.reddit, 'latest' );
     }
-
-
 
     $scope.getEntertainmentWeekly = function() {
         $scope.entertainmentWeekly = [];
-        getNews('entertainment-weekly', $scope.entertainmentWeekly );
+        getNews('entertainment-weekly', $scope.entertainmentWeekly, 'top' );
     }
 
     $scope.getDailyMail = function() {
         $scope.dailyMail = [];
-        getNews('daily-mail', $scope.dailyMail );
+        getNews('daily-mail', $scope.dailyMail, 'latest' );
     }
 
-    var getNews = function (source, arrayName) {
+    var getNews = function (source, arrayName, topOrLatest) {
         $http({
             method: 'GET',
-            url: 'https://newsapi.org/v1/articles?source=' + source + '&sortBy=top&apiKey=79b5eacee55a4e8d89ed43a73add9edf'
+            url: 'https://newsapi.org/v1/articles?source=' + source + '&sortBy=' + topOrLatest +'&apiKey=79b5eacee55a4e8d89ed43a73add9edf'
         }).then(function successCallback(response) {
             Array.prototype.push.apply(arrayName, response.data.articles);
             console.log(source + arrayName + $scope.articles);
